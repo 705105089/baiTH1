582119f63ab6da1624f2fa62cebfeea56e338798
phuong <justaphotovn@gmail.com>
2023-02-28 20:26:33 +0700

    Phien ban 2

--- code.cpp
+++ code.cpp
@@ -4,8 +4,14 @@ void sap_xep_day_so (int* a, int n)
 {
 	//ma lenh
 }
+
+int tim_max (int* a, int n)
+{
+	//ma lenh
+}
+
 int main()
 {
-	cout << "Phien ban dau tien";
+	cout << "Phien ban 2";
 	return 0;
 }
